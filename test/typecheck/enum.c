0aa3722c0b6d71cd4e79939b24f6c8db5c680c87
Lai-YT <381xvmvbib@gmail.com>
2025-02-27 14:43:05 +0800

    Check type of enum
    With the introduction of enums, the concept of constant expressions must
    also be defined.
    Ideally, a `ConstExprNode` would wrap the `IdExprNode` representing an
    enumeration constant. However, to keep the parser simple without
    maintaining symbol tables, it cannot determine at parse time whether an
    identifier refers to an enumeration constant.
    Additionally, modifying the AST to insert a new node above `IdExprNode`
    is incompatible with our visitor pattern.
    To address this, `IdExprNode` now holds a `ConstExprNode`, a newly
    introduced node currently represented by `IntConstExprNode`.
    The type checker determines if an identifier is an enumeration constant
    by consulting the symbol table. If so, it constructs a `ConstExprNode`
    with the corresponding constant value.

---
+++ test/typecheck/enum.c
@@ -0,0 +1,34 @@
+enum {
+  TRUE,
+  FALSE,
+};
+
+int main() {
+  int f = FALSE;
+
+  enum Color {
+    RED,  // 0
+    GREEN = 2,
+    BLUE,
+  };
+
+  // Convertible to int.
+  int r = RED;
+  enum Color b = GREEN;
+  int c = b;
+
+  // Enum constants are visible in the scope even if it's declared with a
+  // variable.
+  enum {
+    MONDAY = 1,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY = 5,
+    SATURDAY,
+    SUNDAY,
+  } w = MONDAY;
+  int t = TUESDAY;
+
+  return 0;
+}
